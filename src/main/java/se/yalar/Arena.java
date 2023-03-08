bf82a60a4ec17549b37957f8a3c9425cc832c252
Simon Ulander <devsimonulander@gmail.com>
2023-03-09 00:26:45 +0100

    Made Arena.

--- src/main/java/se/yalar/Arena.java
+++ src/main/java/se/yalar/Arena.java
@@ -1,6 +1,52 @@
 package se.yalar;
 
-public class Arena {
-    //test
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
+public class Arena {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String address;
+    private String type; // Ex: inside/outside/etc...
+
+    public Arena() {
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 }
